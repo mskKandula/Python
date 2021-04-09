d1b5b2559d4beea500d1b73ee361d15a7d6840c7
mskKandula <mskKandula@gmail.com>
2021-04-09 11:09:37 +0530

    Image Text Extraction using OCR

---
+++ TextExtraction/Image/image.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import pytesseract
+
+# pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
+# Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
+def ocr_core(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    return text
+
+print(ocr_core('./download.png'))
