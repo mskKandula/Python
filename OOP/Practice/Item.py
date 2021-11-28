fcac5f9bd11259c8f9a03478e9eac96f59302bc7
mohanak <mohanak@mkcl.org>
2021-11-28 11:53:31 +0530

    Custom Error Messages on assert

--- OOP/Practice/Item.py
+++ OOP/Practice/Item.py
@@ -2,8 +2,8 @@ class Item:
     def __init__(self,name:str, price:float, quantity=0) -> None:
         
         #Run Validations on received Arguments
-        assert price >=0
-        assert quantity >=0
+        assert price >=0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >=0, f"Quantity {quantity} is not greater than or equal to zero!"
 
         #Assigning values to self object
         self.name = name
@@ -14,9 +14,9 @@ class Item:
         return self.price * self.quantity
 
 
-item1 = Item("Phone",100,5)
+item1 = Item("Phone",100,-5)
 
-item2 = Item("Laptop",200,3)
+item2 = Item("Laptop",-200,3)
 
 print(item1.calculateTotalPrice())
 
